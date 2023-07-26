cb8e8b5fd0e3ddb7eacd0113f939cf0de9717ca5
DiaoYaohui <57289786+DiaoYaohui@users.noreply.github.com>
2023-07-26 18:01:37 +0800

    Update clewd.js

--- clewd.js
+++ clewd.js
@@ -2,7 +2,7 @@
  * SET YOUR COOKIE HERE
  * @preserve
  */
-const Cookie = '';
+const Cookie = 'process.env.cookies';
 
 /**
 ## EXPERIMENTAL
@@ -77,7 +77,7 @@ const Settings = {
     StripHuman: false
 };
 
-const Ip = '127.0.0.1';
+const Ip = '0.0.0.0';
 const Port = 8444;
 
 /**
